b7e5a69c1f2b2ab7d50b8236f2c295c29bdb8c1b
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-06-29 02:18:32 +0530

    ........______.....

--- SEMISTER.c
+++ SEMISTER.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-union employee{
+/*union employee{
     char name[40];
     int salary;
     int income;
@@ -15,3 +15,38 @@ int main(){
     printf("%d\n",e1.salary);
     return 0;
 }
+    
+   enum days{
+    monday,
+    tuesday,
+    wednesday
+   };
+   int main(){
+    enum days today;
+    today = tuesday;
+    printf("%d",today);
+    return 0;
+   }
+    */
+   enum traficsignal{
+    RED,
+    YELLOW,
+    GREEN
+   };
+   int main(){
+    enum traficsignal signal = RED;
+    switch(signal){
+        case RED:
+        printf("STOP\n");
+        break;
+        case YELLOW:
+        printf("START\n");
+        break;
+        case GREEN:
+        printf("GO\n");
+        break;
+    }
+    return 0;
+   }
+
+   
