f3aae49cf4a7a9652739e562a3f7f0b7d341ac67
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-06-29 01:54:29 +0530

    .....

--- SEMISTER.c
+++ SEMISTER.c
@@ -1,1 +1,17 @@
-/* semister preparation purpose*/
+#include<stdio.h>
+#include<string.h>
+union employee{
+    char name[40];
+    int salary;
+    int income;
+};
+int main(){
+    union employee e1;
+    strcpy(e1.name,"chetan");
+    e1.salary = 100000;
+    e1.income = 200000;
+    printf("%d\n",e1.income);
+    printf("%s\n",e1.name);
+    printf("%d\n",e1.salary);
+    return 0;
+}
