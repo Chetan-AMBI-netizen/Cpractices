240dc77d0d6769ab74a315f121e48b52de764826
Chetan-AMBI-netizen <chetanambiger0@gmail.com>
2026-06-30 05:46:42 +0530

    :::::::::

--- SEMISTER.c
+++ SEMISTER.c
@@ -233,7 +233,7 @@ for( i=0; i<n; i++){
         return 0;
 
     }
-        */
+        
        int main(){
         int a,b,c;
         int d,root1,root2;
@@ -263,3 +263,29 @@ for( i=0; i<n; i++){
         }
        return 0;
        }
+*/
+int prime(int n){
+    int i;
+    if(n<=1){
+        return 0; 
+    }
+    for(i=2; i<=n/2; i++){
+        if(n%i == 0){
+            return 0;
+        }
+    }
+    return 1 ;
+
+  
+}
+int main(){
+    int num;
+    printf("enter number:");
+    scanf("%d", &num);
+    if(prime(num))
+    printf("%d is prime number",num);
+    else{
+        printf("%d is not a prime number", num);
+    }
+    return 0;
+}
